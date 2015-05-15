80b5626515d34a3f44f40876c936d0773292766e
enerjiser2 <project@idefclub.ru>
2015-05-15 20:22:09 +0300

    Добавил трейт с магическими методами __get и __set для работы с несуществующими свойствами объекта

---
+++ Traits/TMagicGetSet.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Traits;
+
+trait TMagicGetSet
+{
+    protected $magicProp = [];
+
+    public function __get($name)
+    {
+        return $this->magicProp[$name];
+    }
+
+    public function __set($name, $value)
+    {
+        $this->magicProp[$name] = $value;
+    }
+}
